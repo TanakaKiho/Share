6a5029e8055617ca0cc32c328878540d7f43ba35
TanakaKiho <moon.kiho.premier@gmail.com>
2022-12-15 15:27:23 +0900

    Update stable_diffusionDemo.py

--- AI関連/stable_diffusionDemo.py
+++ AI関連/stable_diffusionDemo.py
@@ -9,8 +9,14 @@ YOUR_TOKEN = "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"
 pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, revision="fp16", torch_dtype=torch.float16, use_auth_token=YOUR_TOKEN)
 pipe.to(DEVICE)
  
-prompt = "a dog painted by Katsuhika Hokusai"
+prompt = "star"
  
+def null_safety(images,**kwargs):
+  return images,False
+
+pipe.safety_checker = null_safety
 with autocast(DEVICE):
-  image = pipe(prompt, guidance_scale=7.5)["sample"][0]
+  #image = pipe(prompt, guidance_scale=7.5)["sample"][0]
+  #image = pipe(prompt)["sample"][0]
+  image = pipe(prompt).images[0]
   image.save("test.png")
